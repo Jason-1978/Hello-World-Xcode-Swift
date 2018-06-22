1e316badbde7ca290ce82c00627e303a90209d99
Jason Kralik <jgkra123@gmail.com>
2018-06-22 14:33:32 -0400

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jason Kralik on 6/22/18.
+//  Copyright © 2018 Jason Kralik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
